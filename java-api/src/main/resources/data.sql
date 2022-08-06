384de7325a6f623de113b29183f650be0d8f4e6e
nachiket kanore <nachiket.kanore@gmail.com>
2022-08-06 14:59:12 +0530

    made it to work on my machine: version changes and sql statements added

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO dogs (id, name, age) VALUES (1, 'Einstein', 3);
 INSERT INTO dogs (id, name, age) VALUES (2, 'Kaya', 5);
+INSERT INTO dogs (id, name, age) VALUES (3, 'Max', 9);
+INSERT INTO dogs (id, name, age) VALUES (4, 'Harry', 2);
+INSERT INTO dogs (id, name, age) VALUES (5, 'Steph', 5);
+INSERT INTO dogs (id, name, age) VALUES (6, 'Ben', 1);
