242c8cdd8baf6491d7631f164a9a4e1dd85bcbda
nachiket kanore <nachiket.kanore@gmail.com>
2022-08-09 16:52:14 +0530

    Crossorigin global configuration
    Not sure if it works or not..

--- java-api/src/main/java/com/db/grad/javaapi/JavaApiApplication.java
+++ java-api/src/main/java/com/db/grad/javaapi/JavaApiApplication.java
@@ -2,12 +2,26 @@ package com.db.grad.javaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@RestController
 public class JavaApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaApiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
 }
