33a9eaa2202d7e31e6da32f25af6ea1a96525dac
nachiket kanore <nachiket.kanore@gmail.com>
2022-08-10 13:35:23 +0530

    added tailwind config

--- react-app/src/components/Securities.js
+++ react-app/src/components/Securities.js
@@ -37,32 +37,32 @@ const Securities = () => {
     }
 
     return (
-      <div class="flex flex-col">
-        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="overflow-hidden">
-              <table class="min-w-full">
-                <thead class="border-b">
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full">
+                <thead className="border-b">
                   <tr>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-centre ">
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-centre ">
                       Id
                     </th>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-centre">
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-centre">
                       type
                     </th>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-centre">
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-centre">
                       status
                     </th>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-centre">
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-centre">
                       faceValue
                     </th>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-centre">
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-centre">
                       MaturityDate
                     </th>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-centre">
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-centre">
                       coupon
                     </th>
-                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-centre">
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-centre">
                       cusip
                     </th>
                   </tr>
@@ -72,26 +72,26 @@ const Securities = () => {
                   {security.map((obj, ind) => {
                     {/* <button>  */ }
                     return (
-                      <tr class="border-b">
+                      <tr className="border-b">
                       <button onClick={ (e)=>handlerFunction(ind)} >
-                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 "  >{obj.id}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 "  >{obj.id}</td>
                         </button>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                           {obj.type}
                         </td>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                           {obj.status}
                         </td>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                           {obj.faceValue}
                         </td>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                           {obj.maturityDate}
                         </td>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                           {obj.coupon}
                         </td>
-                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                           {obj.cusip}
                         </td>
                       </tr>
@@ -99,39 +99,39 @@ const Securities = () => {
                   })}
 
 
-                  {/* <tr class="border-b">
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  {/* <tr className="border-b">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                       M
                     </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                       O
                     </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                       @
                     </td>
                   </tr>
-                  <tr class="bg-white border-b">
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  <tr className="bg-white border-b">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                       J
                     </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                       T
                     </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                       @f
                     </td>
                   </tr>
-                  <tr class="bg-white border-b">
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  <tr className="bg-white border-b">
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                       L
                     </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                       W
                     </td>
-                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                       @t
                     </td>
                   </tr> */}
